808cfc1434f99bf46cdd4503872cc0dcae12c9dd
Lars.Im.Netz <lars.langhans@gmx.de>
2013-12-02 20:32:29 +0100

    Slider mit ein paar Extra Parametern

--- src/main/java/de/hypoport/einarbeitung/SliderPanel.java
+++ src/main/java/de/hypoport/einarbeitung/SliderPanel.java
@@ -12,7 +12,7 @@ public class SliderPanel extends Panel {
 	public SliderPanel(String id) {
 		super(id);
 		
-		final Form<Integer> form = new Form<Integer>("form", new Model<Integer>(15));
+		final Form<Integer> form = new Form<Integer>("form", new Model<Integer>(80));
 		this.add(form);
 
 
@@ -20,7 +20,12 @@ public class SliderPanel extends Panel {
 		final Label label = new Label("label", form.getModel()); //the supplied model allows the initial display
 		form.add(label);
 		
-		form.add(new Slider("slider", form.getModel(), label)); //will use the html markupId
+		Slider slider = new Slider("slider", form.getModel(), label);
+		slider.setMin(50);
+		slider.setMax(200);
+		slider.setStep(10);
+		// slider.setRange(range);
+		form.add(slider); //will use the html markupId
 		
 	}
 
