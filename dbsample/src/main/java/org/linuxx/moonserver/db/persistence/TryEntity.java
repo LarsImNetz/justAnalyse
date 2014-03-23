926ee64cf09899e56c2d4e271ea7a3146bf72783
Lars.Im.Netz <lars.langhans@gmx.de>
2014-03-23 18:29:04 +0100

    try code gen

--- dbsample/src/main/java/org/linuxx/moonserver/db/persistence/TryEntity.java
+++ dbsample/src/main/java/org/linuxx/moonserver/db/persistence/TryEntity.java
@@ -10,21 +10,21 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "Try")
-@Cacheable(false)
+// @Cacheable(false)
 public class TryEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 
 	@Column(nullable = false, length = 255)
 	private String name;
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
