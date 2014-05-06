a5fa1027cb15544e41457c32e28e1d17e926dab5
Lars.im.Netz <java4fun@kaffeeschluerfer.com>
2014-05-06 22:09:47 +0200

    HTML 5 Tests

---
+++ html5tests/src/main/java/org/linuxx/moonserver/HomePage.java
@@ -0,0 +1,50 @@
+package org.linuxx.moonserver;
+
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.util.string.StringValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HomePage extends WebPage {
+
+	private final static Logger logger = LoggerFactory.getLogger(HomePage.class);
+
+	@SuppressWarnings("serial")
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+//		logger.debug("HomePage of DateViewer");
+//
+//		IModel<String> textToView = new LoadableDetachableModel<String>() {
+//
+//			@Override
+//			protected String load() {
+//				final String textToString = "Heute ist " + new Date();
+//				logger.debug("Zu zeigender Text: " + textToString);
+//				return textToString;
+//			}
+//
+//		};
+
+		StringValue value = parameters.get("a");
+		
+		final Label label = new Label("label", value.toString());
+		add(label);
+	}
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		final JavaScriptResourceReference omnitureScript = new JavaScriptResourceReference(HomePage.class, "file.js");
+
+		response.render(JavaScriptHeaderItem.forReference(omnitureScript));
+		response.render(OnDomReadyHeaderItem.forScript("funktion();"));
+	}
+}
---
+++ html5tests/src/main/java/org/linuxx/moonserver/file.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+
+function funktion() {
+	alert("just funktion().");
+}
