d58003a2d0702067972f2aea429b3ae79dc3b2b6
Lars.im.Netz <java4fun@kaffeeschluerfer.com>
2014-05-06 21:59:18 +0200

    Ein paar Tests mit JAva 8 und Javascript

---
+++ javaeight/src/test/java/org/linuxx/moonserver/TestEight.java
@@ -0,0 +1,27 @@
+package org.linuxx.moonserver;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestEight {
+
+	@Test
+	public void test() {
+
+		List<Integer> asList = Arrays.asList(10,20,30,40,50,60,70,80);
+		Stream<Integer> stream = asList.stream();
+		double sum = stream.filter(value -> value > 40)
+		.mapToDouble(value -> value * 1.0)
+		.sum();
+
+		Assert.assertEquals(50+60+70+80d, sum, 0.01);
+		System.out.println("Summe: " + sum);
+	}
+
+}
---
+++ javaeight/src/test/java/org/linuxx/moonserver/TestHomePage.java
@@ -0,0 +1,31 @@
+package org.linuxx.moonserver;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+import org.linuxx.moonserver.HomePage;
+import org.linuxx.moonserver.WicketApplication;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestHomePage
+{
+	private WicketTester tester;
+
+	@Before
+	public void setUp()
+	{
+		tester = new WicketTester(new WicketApplication());
+	}
+
+	@Test
+	public void homepageRendersSuccessfully()
+	{
+		//start and render the test page
+		tester.startPage(HomePage.class);
+
+		//assert rendered page class
+		tester.assertRenderedPage(HomePage.class);
+	}
+}
