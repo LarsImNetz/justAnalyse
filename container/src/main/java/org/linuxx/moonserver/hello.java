679df73b5e857dc53ab5ebb5a0ce5d656d754e43
Lars Langhans <lars.langhans@hypoport.de>
2014-12-09 09:31:24 +0100

    Add Guava

--- container/src/main/java/org/linuxx/moonserver/hello.java
+++ container/src/main/java/org/linuxx/moonserver/hello.java
@@ -1,7 +1,10 @@
 package org.linuxx.moonserver;
 
+import com.google.common.base.Preconditions;
+
 public class hello  {
     public static void main( String[] argv ) {
-        System.out.println("Hello World");
+		Preconditions.checkArgument(argv != null);
+    	System.out.println("Hello World");
     }
 }
