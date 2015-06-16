fc4e628f3e8e3a5d2fc6d67b2197573d72ce43de
Lars Langhans <lars.langhans@hypoport.de>
2015-06-16 16:15:45 +0200

    dbsample repaired

--- dbsample/src/test/java/org/linuxx/moonserver/db/persistence/TestTryDao.java
+++ dbsample/src/test/java/org/linuxx/moonserver/db/persistence/TestTryDao.java
@@ -3,6 +3,7 @@ package org.linuxx.moonserver.db.persistence;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
@@ -30,30 +31,39 @@ public class TestTryDao {
 
 		dao = new TryDao();
 		injector.injectMembers(dao);
-	
-		dao2 = injector.getInstance(Try2Dao.class);	
+
+		dao2 = injector.getInstance(Try2Dao.class);
 	}
 
 	@Before
-	public void fillDB() {
+	public void fillTable() {
+
+		// tipp: versuche es nicht mit '0' als ID, das geht schief
+		// http://stackoverflow.com/questions/22026715/jpa-null-or-zero-primary-key-encountered-in-unit-of-work-clone
+		// <property name="eclipselink.allow-zero-id" value="true"/>
 		TryEntity entity = new TryEntity();
-		entity.setId(0);
+		entity.setId(1);
 		entity.setName("testname");
 		dao.save(entity);
 
 		dao.deleteAll();
-		
+
 		TryEntity entity2 = new TryEntity();
-		entity2.setId(0);
+		entity2.setId(1);
 		entity2.setName("testname");
 		dao.save(entity2);
-		
 	}
+
+	@After
+	public void clearTable() {
+		dao.deleteAll();
+	}
+
 	@AfterClass
 	public static void cleanUpAfterClass() {
 		EntityManager em = injector.getInstance(EntityManager.class);
 		EntityManagerFactory factory = em.getEntityManagerFactory();
-		
+
 		em.clear();
 		em.close();
 		factory.close();
@@ -61,24 +71,29 @@ public class TestTryDao {
 
 	@Test
 	public void testDao() throws Exception {
-		TryEntity entity = dao.fetch(0);
+		TryEntity entity = dao.fetch(1);
 		Assert.assertEquals("testname", entity.getName());
 	}
 
 	@Test
 	public void testTry2Entity() {
 		Try2Entity entity = new Try2Entity();
-		entity.setId(1);
+		entity.setId(3);
 		entity.setName("zweiter Name");
 		dao2.save(entity);
 
-		Try2Entity entity2 = dao2.fetch(1);
+		Try2Entity entity2 = dao2.fetch(3);
 		entity2.setName("geändert");
 		dao2.save(entity2);
 
-		Try2Entity entity3 = dao2.fetch(1);
+		Try2Entity entity3 = dao2.fetch(3);
 		Assert.assertEquals("geändert", entity3.getName());
+	}
 
+	@Test
+	public void testName() {
+		TryEntity fetch = dao.fetch(1);
+		Assert.assertEquals("testname", fetch.getName());
 	}
 
 	private static class TryDaoTestModule extends AbstractModule {
@@ -86,7 +101,7 @@ public class TestTryDao {
 		@Override
 		protected void configure() {
 			bind(EntityManager.class).toProvider(EntityManagerTestProvider.class);
-			
+
 			bind(ITryDao.class).to(TryDao.class);
 
 			bind(ITry2Dao.class).to(Try2Dao.class);
