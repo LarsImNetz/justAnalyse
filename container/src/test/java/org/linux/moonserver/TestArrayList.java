2d6cfdc9fc02a7d6a331ce615695b33e5487479e
Lars Langhans <lars.langhans@hypoport.de>
2015-09-02 16:11:39 +0200

    Hinzufügen von etlichen Tests für ArrayList

--- container/src/test/java/org/linux/moonserver/TestArrayList.java
+++ container/src/test/java/org/linux/moonserver/TestArrayList.java
@@ -51,10 +51,60 @@ public class TestArrayList {
 
 	@Test
 	public void testAddAll() throws Exception {
-		final List<String> asList = Arrays.asList("eins", "zwei", "drei");		
+		final List<String> asList = Arrays.asList("eins", "zwei", "drei");
 		list.addAll(asList);
 		Assert.assertEquals(6, list.size());
 		Assert.assertEquals("eins", list.get(3));
+	}
+
+	@Test
+	public void testSet() {
+		list.set(0, "neu");
+		final String first = list.get(0);
+		assertEquals("neu", first);
+	}
+
+	@Test
+	public void testContains() {
+		Assert.assertTrue(list.contains("a"));
+		Assert.assertFalse(list.contains("d"));
+	}
+
+	@Test
+	public void testIsEmpty() {
+		Assert.assertTrue(new ArrayList<String>().isEmpty());
+		Assert.assertFalse(list.isEmpty());
+	}
+
+	@Test
+	public void testIndexOf() {
+		Assert.assertEquals(0, list.indexOf("a"));
+		Assert.assertEquals(1, list.indexOf("b"));
+		Assert.assertEquals(2, list.indexOf("c"));
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		list.remove(0);
+		Assert.assertEquals(2, list.size());
+	}
+
+	@Test
+	public void testRemoveAll() throws Exception {
+		list.removeAll(Arrays.asList("a", "b"));
+		Assert.assertEquals(1, list.size());
+	}
+
+	@Test
+	public void testClear() throws Exception {
+		list.clear();
+		Assert.assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void testListErstellen() throws Exception {
+		final List<String> list = new ArrayList<String>(Arrays.asList( "0 1 2 3 4 5 6 7 8 9".split( " " ) ));
+		Assert.assertEquals(10, list.size());
 	}
 	
 	@Test
