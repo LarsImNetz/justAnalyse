1efadde1b676eed4adfa9829a1d377d3c8e44aa7
Lars Langhans <lars.langhans@hypoport.de>
2015-09-02 15:52:16 +0200

    Mehr Tests in Listen

--- container/src/test/java/org/linux/moonserver/TestArrayList.java
+++ container/src/test/java/org/linux/moonserver/TestArrayList.java
@@ -1,6 +1,7 @@
 package org.linux.moonserver;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
@@ -39,6 +40,21 @@ public class TestArrayList {
 	public void testMoreSize() throws Exception {
 		list.add("d");
 		Assert.assertEquals(4, list.size());
+	}
+
+	@Test
+	public void testAddWithPosition() throws Exception {
+		list.add(0, "eins");
+		Assert.assertEquals(4, list.size());
+		Assert.assertEquals("eins", list.get(0));
+	}
+
+	@Test
+	public void testAddAll() throws Exception {
+		final List<String> asList = Arrays.asList("eins", "zwei", "drei");		
+		list.addAll(asList);
+		Assert.assertEquals(6, list.size());
+		Assert.assertEquals("eins", list.get(3));
 	}
 	
 	@Test
