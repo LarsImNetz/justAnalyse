11e86b24db147d3bb26bdc70965704fc89d2f570
Lars Langhans <lars.langhans@hypoport.de>
2016-01-26 11:59:28 +0100

    Integrationstest

---
+++ konto-import/src/test/java/org/homenet/moonserver/kontoimporter/buchung/ITBuchen.java
@@ -0,0 +1,37 @@
+package org.homenet.moonserver.kontoimporter.buchung;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.homenet.moonserver.kontoimporter.CSVDirectoryReader;
+import org.homenet.moonserver.kontoimporter.CSVKontoInterpreter;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ITBuchen {
+
+	// Wir laufen alles durch, es gibt in jeder Datei in jedem Format Buchungen
+	
+	@Test
+	public void testBuchen() {
+		final CSVDirectoryReader reader = new CSVDirectoryReader(
+				"src/test/resources/org/homenet/moonserver/kontoimporter");
+		final Collection<Object[]> csvFiles = reader.findAllCSVFiles();
+
+		Assert.assertEquals(1, csvFiles.size());
+
+		final Iterator<Object[]> iterator = csvFiles.iterator();
+		while (iterator.hasNext()) {
+			final Object[] csvFileObject = iterator.next();			
+			final File csvFile = (File)csvFileObject[0];		
+		
+			final CSVKontoInterpreter interpreter = new CSVKontoInterpreter(csvFile);
+			final List<Buchung> buchungen = interpreter.interpret();
+
+			Assert.assertNotNull(buchungen);
+		}
+	}
+}
