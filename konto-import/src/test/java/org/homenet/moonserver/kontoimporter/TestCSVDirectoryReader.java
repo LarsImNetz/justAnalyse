4b9603a39264299cae566d00aa9cece92981e717
Lars Langhans <lars.langhans@hypoport.de>
2016-02-09 08:45:59 +0100

    Test erweitert, prüft jetzt auch die existance

--- konto-import/src/test/java/org/homenet/moonserver/kontoimporter/TestCSVDirectoryReader.java
+++ konto-import/src/test/java/org/homenet/moonserver/kontoimporter/TestCSVDirectoryReader.java
@@ -8,12 +8,22 @@ import org.junit.Test;
 
 public class TestCSVDirectoryReader {
 
-	// Dieser Test wird fehlschlagen, sobald mehr test*.csv Dateien in den
-	// resourcen untergebracht werden
+	private static String baseFolder = "src/test/resources/org/homenet/moonserver/kontoimporter";
+
+	@Test
+	public void testCheckExistance() {
+		final File baseFolderFile = new File(baseFolder);
+		Assert.assertTrue("base folder must exist.", baseFolderFile.exists());
+	}
+
+	/**
+	 * Dieser Test wird fehlschlagen, sobald mehr test*.csv Dateien in den
+	 * Ressourcen untergebracht werden
+	 */
 	@Test
 	public void testCountConfigurationFiles() throws Exception {
-		String baseFolder = "src/test/resources/org/homenet/moonserver/kontoimporter";
-		File baseFolderFile = new File(baseFolder);
+		final File baseFolderFile = new File(baseFolder);
+
 		final CSVDirectoryReader reader = new CSVDirectoryReader(baseFolderFile, new SimpleCSVFilter());
 		final Collection<Object[]> csvFiles = reader.findAllCSVFiles();
 
