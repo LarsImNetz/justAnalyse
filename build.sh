fe51f9f3ad506581c3e22777d4b4adaeaeac7b66
Lars Langhans <lars.langhans@hypoport.de>
2016-04-29 08:45:02 +0200

    build script, das zeigt, wie ein Maven Projekt erstellt werden kann.

---
+++ build.sh
@@ -0,0 +1,4 @@
+# Script hat Democharakter, es soll zeigen, wie man neue Maven Projekte anlegen kann.
+# create mongodb pom.xml
+# mvn archetype:generate -DgroupId=privat.lla -DartifactId=mongodb -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
