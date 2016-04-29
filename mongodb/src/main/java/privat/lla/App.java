ff5b9bec1eb2ffd0ca0348cdf397a86f88427c4a
Lars Langhans <lars.langhans@hypoport.de>
2016-04-29 08:41:47 +0200

    cleanup, code minimalisiert

--- mongodb/src/main/java/privat/lla/App.java
+++ mongodb/src/main/java/privat/lla/App.java
@@ -1,8 +1,5 @@
 package privat.lla;
 
-import java.util.List;
-import java.util.Set;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -16,73 +13,47 @@ import com.mongodb.MongoClient;
  */
 public class App {
 
-	public static void main(final String[] args) throws Exception {
+	public static void main(final String[] args) {
 
 		/*
 		 * MongoDB Example entnommen:
 		 * http://www.mkyong.com/mongodb/java-mongodb-hello-world-example/
 		 */
+
 		/*
 		 * 3. Mongo Connection
+		 * Since 2.10.0, uses MongoClient
 		 */
-		// Old version, uses Mongo
-		// final Mongo mongo = new Mongo("rzsolv163.rz-hypoport.local", 27000);
+		MongoClient mongo = null;
+		try {
+			mongo = new MongoClient("solv262.hypoport.local", 27000);
+			// final MongoClient mongo = new MongoClient("rzsolv163.rz-hypoport.local", 27000);
 
-		// Since 2.10.0, uses MongoClient
-		final MongoClient mongo = new MongoClient("solv262.hypoport.local", 27000);
-		// final MongoClient mongo = new MongoClient("rzsolv163.rz-hypoport.local", 27000);
+			/*
+			 * Mongo Database
+			 */
+			final DB db = mongo.getDB("baushup-antrag-QA");
 
-		/*
-		 * Mongo Database
-		 */
-		final DB db = mongo.getDB("baushup-antrag-QA");
+			// from interest:
+			// {"bauobjekt": {"ort": {"plz": plz, "ort": ort}}}
 
-		/*
-		 * List all Databases
-		 */
-		System.out.println("Zeige alle Datenbank-Namen");
-		final List<String> dbs = mongo.getDatabaseNames();
-		for (final String db1 : dbs) {
-			System.out.println(db1);
+			System.out.println("baugeld Content");
+			final DBCollection baugeldCollection = db.getCollection("baugeld");
+			final DBCursor cursor = baugeldCollection.find();
+
+			while (cursor.hasNext()) {
+				final DBObject next = cursor.next();
+				show(next);
+			}
+			cursor.close();
+			mongo.close();
 		}
-		/*
-		 * Mongo Collection
-		 */
-		// final DB db2 = mongo.getDB("baushup-antrag-test");
-		final DBCollection table = db.getCollection("user");
-		/*
-		 * Display all Collections
-		 */
-		// final DB db3 = mongo.getDB("testdb");
-		final Set<String> tables = db.getCollectionNames();
-		System.out.println("Collection Names");
-		for (final String coll : tables) {
-			System.out.println(coll);
+		catch (final Exception e) {
+
 		}
-
-		// from interest:
-		// {"bauobjekt": {"ort": {"plz": plz, "ort": ort}}}
-
-		System.out.println("baugeld Content");
-		final DBCollection baugeldCollection = db.getCollection("baugeld");
-		final DBCursor cursor = baugeldCollection.find();
-		// final List<DBObject> dbobjects = cursor.toArray();
-
-		while (cursor.hasNext()) {
-			final DBObject next = cursor.next();
-			show(next);
-			// System.out.println(next.toString());
+		finally {
+			mongo.close();
 		}
-		cursor.close();
-		mongo.close();
-		/*
-		 * DBCollection table = db.getCollection("user");
-		 * BasicDBObject document = new BasicDBObject();
-		 * // document.put("name", "mkyong");
-		 * document.put("age", 30);
-		 * document.put("createdDate", new Date());
-		 * table.insert(document);
-		 */
 	}
 
 	private static void show(final DBObject obj) {
