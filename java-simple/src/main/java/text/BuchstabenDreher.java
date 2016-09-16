995fff8bb218e6aff23a40c51e64eb5e1cce3bd8
Lars Langhans <lars.langhans@hypoport.de>
2016-09-16 15:03:05 +0200

    BuchstabenDreher um Preconditions erweitert

--- java-simple/src/main/java/text/BuchstabenDreher.java
+++ java-simple/src/main/java/text/BuchstabenDreher.java
@@ -1,5 +1,7 @@
 package text;
 
+import com.google.common.base.Preconditions;
+
 /*
  * Buchstabendreher
  * Verdreht und Vertauscht buchstaben in einer beliebigen Reihenfolge
@@ -10,6 +12,9 @@ public class BuchstabenDreher {
 	private BuchstabenManipulator manipulator;
 	
 	public BuchstabenDreher(String word) {
+		Preconditions.checkNotNull(word, "Bitte ein Wort angeben.");
+		Preconditions.checkArgument(word.indexOf(" ") == -1, "Bitte nur einzelne Wörter angeben!");
+		
 		this.word = word;
 		manipulator = new BuchstabenManipulator(word);
 	}
@@ -18,22 +23,24 @@ public class BuchstabenDreher {
 		return manipulator.getWord();
 	}
 	
-	public void verdrehen() {
+	public BuchstabenDreher verdrehen() {
 		// Wörter mit weniger als 4 Buchstaben lassen sich nicht verdrehen.
 		if (word.length() < 4) {
-			return;
+			return this;
 		}
 
 		manipulator.buchstabenNachbarnTauschen();
+		return this;
 	}
 
-	public void verwuerfeln() {
+	public BuchstabenDreher verwuerfeln() {
 		// Wörter mit weniger als 4 Buchstaben lassen sich nicht tauschen.
 		if (word.length() < 4) {
-			return;
+			return this;
 		}
 	
 		manipulator.buchstabenZweitenMitLetztemTauschen();
+		return this;
 	}
 	
 }
