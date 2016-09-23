c21c2285b2a9307ec67594f492e2e9bccb707023
Lars Langhans <lars.langhans@hypoport.de>
2016-09-23 08:35:57 +0200

    Längerer Satz

--- java-simple/src/test/java/text/TestSatzDreher.java
+++ java-simple/src/test/java/text/TestSatzDreher.java
@@ -16,4 +16,11 @@ public class TestSatzDreher {
 		SatzDreher satzDreher = new SatzDreher("Dieser Satz sollte noch lesbar sein, auch wenn er etwas komisch aussieht.");
 		System.out.println(satzDreher.getVerdrehtenSatz());
 	}
+	
+	@Test
+	public void testLongSentence() {
+		String satz = "Wer jetzt bereits die Datenbank gefüllt hat, sollte diese bereinigen, nach dem die Stopwörter gesetzt wurden. Mit einem einfachen SQL-Statement kann die Relationen-Tabelle aufgeräumt werden. Dabei ist zu berücksichtigen, dass bei mehreren Millionen Datensätzen dieser Vorgang recht lange dauern kann. Ich empfehle daher die Abfrage mit einem LIMIT auszuführen, so dass nur eine begrenzte Zahl an Datensätzen gelöscht wird. Der Vorgang muss dann mehrfach wiederholt werden. Dabei kann LIMIT entsprechend der Reaktionszeit des Servers angepasst werden. Wer damit noch kein Erfahrung hat, sollte ggf. jemanden fragen, der sich ein wenig mit SQL auskennt.";
+		SatzDreher satzDreher = new SatzDreher(satz);
+		System.out.println(satzDreher.getVerdrehtenSatz());		
+	}
 }
