2c20aada61c05b6f1284a3ccb58e148a8278e69d
Lars Langhans <lars.langhans@hypoport.de>
2016-11-09 16:02:20 +0100

    some more tests with reduce

--- javaeight/src/test/java/org/linuxx/moonserver/map/reduce/TestReduce.java
+++ javaeight/src/test/java/org/linuxx/moonserver/map/reduce/TestReduce.java
@@ -9,13 +9,51 @@ import org.junit.Test;
 
 public class TestReduce {
 
+	final List<Integer> asList = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80);
+
 	@Test
-	public void testReduce() {
-		final List<Integer> asList = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80);
+	public void testReduceAsSum() {
 		final Stream<Integer> stream = asList.stream();
 		final Integer summe = stream.reduce(0, (a, b) -> a + b);
 
 		Assert.assertEquals(Integer.valueOf(10 + 20 + 30 + 40 + 50 + 60 + 70 + 80), summe);
 	}
+
+	@Test
+	public void testReduceAsSum2() {
+		final Stream<Integer> stream = asList.stream();
+		Integer count = stream.reduce(0, Integer::sum);
+		
+		Assert.assertEquals(Integer.valueOf(10 + 20 + 30 + 40 + 50 + 60 + 70 + 80), count);		
+	}
+
+	@Test
+	public void testReduceAsCount() {
+		final Stream<Integer> stream = asList.stream();
+		final Integer count = stream.reduce(0, (a, b) -> a + 1);
+
+		Assert.assertEquals(Integer.valueOf(8), count);
+	}
+
+	@Test
+	public void testReduceAsCount2() {
+		final Stream<Integer> stream = asList.stream();
+		final Integer countValue = stream.reduce(0, TestReduce::count);
+
+		Assert.assertEquals(Integer.valueOf(8), countValue);
+	}
+
+	private static int count(int a, int b) {
+        return a + 1;
+    }
+
+	@Test
+	public void testCount() {
+		final Stream<Integer> stream = asList.stream();
+		final long countValue = stream.count();
+
+		Assert.assertEquals(8L, countValue);
+	}
+
 
 }
