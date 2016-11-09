9227c1243bf38706f0a42ac98cd9a6ba5de408e4
Lars Langhans <lars.langhans@hypoport.de>
2016-11-09 16:03:17 +0100

    Some Tests with sum

--- javaeight/src/test/java/org/linuxx/moonserver/map/reduce/TestSum.java
+++ javaeight/src/test/java/org/linuxx/moonserver/map/reduce/TestSum.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class TestSum {
 
 	@Test
-	public void testSum() {
+	public void testSumWithFilter() {
 
 		final List<Integer> asList = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80);
 		final Stream<Integer> stream = asList.stream();
@@ -20,4 +20,33 @@ public class TestSum {
 		// System.out.println("Summe: " + sum);
 	}
 
+	@Test
+	public void testSum() {
+
+		final List<Integer> asList = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80);
+		final Stream<Integer> stream = asList.stream();
+		final double sum = stream.mapToDouble(value -> value * 1.0).sum();
+
+		Assert.assertEquals(10 + 20 + 30 + 40 + 50 + 60 + 70 + 80d, sum, 0.01);
+	}
+
+	@Test
+	public void testSumAsCountWithReturn() {
+
+		final List<Integer> asList = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80);
+		final Stream<Integer> stream = asList.stream();
+		final int n = stream.mapToInt(value -> {return 1;}).sum();
+
+		Assert.assertEquals(8, n);
+	}
+
+	@Test
+	public void testSumAsCount() {
+
+		final List<Integer> asList = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80);
+		final Stream<Integer> stream = asList.stream();
+		final int n = stream.mapToInt(value -> 1).sum();
+
+		Assert.assertEquals(8, n);
+	}
 }
