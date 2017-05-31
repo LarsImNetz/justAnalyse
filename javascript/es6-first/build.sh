ade2d668e4389b0a04f5bc3c7e9256dafb3bae1c
Lars Langhans <lars.langhans@hypoport.de>
2017-05-31 09:03:07 +0200

    ES6 Template

---
+++ javascript/es6-first/build.sh
@@ -0,0 +1,9 @@
+npm init --yes
+
+npm install --save-dev gulp gulp-babel gulp-sourcemaps gulp-concat babel-preset-latest babel-plugin-transform-object-rest-spread babel-plugin-transform-es2015-destructuring browser-sync
+
+mkdir src
+mkdir dist
+touch gulpfile.js
+touch src/index.js
+touch index.html
