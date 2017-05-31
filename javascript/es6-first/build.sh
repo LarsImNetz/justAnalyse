1846dfe7caa6f70a7cdb127c103eb31ae1572095
Lars Langhans <lars.langhans@hypoport.de>
2017-05-31 11:50:00 +0200

    add shebang

--- javascript/es6-first/build.sh
+++ javascript/es6-first/build.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 npm init --yes
 
 npm install --save-dev gulp gulp-babel gulp-sourcemaps gulp-concat babel-preset-latest babel-plugin-transform-object-rest-spread babel-plugin-transform-es2015-destructuring browser-sync
