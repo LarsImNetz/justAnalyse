31e1c4cf8498e9238d083a5415cd652a4c27ab29
Lars Langhans <lars.langhans@hypoport.de>
2017-05-31 11:51:12 +0200

    Formattings

--- javascript/es6-first/gulpfile.js
+++ javascript/es6-first/gulpfile.js
@@ -2,30 +2,37 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync')
+	.create();
 
 gulp.task('build', () => {
 	return gulp.src('./src/**/*.js')
 		.pipe(sourcemaps.init())
 		.pipe(babel({
 			presets: ['es2015'],
-			plugins: ['transform-es2015-destructuring', 'transform-object-rest-spread']
+			plugins: [
+				'transform-es2015-destructuring',
+				'transform-object-rest-spread'
+			]
 		}))
 		.pipe(concat('all.js'))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('serve', ['build'], function(cb) {
+gulp.task('serve', ['build'], function (cb) {
 	browserSync.init({
 		server: {
 			baseDir: '.'
 		}
 	});
 
-    gulp.watch('./src/**/*.js', ['build']);
+	gulp.watch('./src/**/*.js', ['build']);
+	gulp.watch([
+		'./dist/*.js',
+		'./**/*.html'
+	])
+		.on('change', browserSync.reload);
 });
 
-
-gulp.watch(['./dist/*.js', './**/*.html']).on('change', browserSync.reload);
 
