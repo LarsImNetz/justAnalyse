df15984433942ed9f54a8f304698e9cacfd4c27f
Lars Langhans <lars.langhans@hypoport.de>
2018-02-20 16:58:06 +0100

    try to create a java8 junit 4 mvn template
    -

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # Script hat Democharakter, es soll zeigen, wie man neue Maven Projekte anlegen kann.
 # create mongodb pom.xml
 # mvn archetype:generate -DgroupId=privat.lla -DartifactId=mongodb -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-
+# mvn archetype:generate -DgroupId=privat.lla -DartifactId=fail-tests    -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+mvn archetype:generate -DgroupId=privat.lla -DartifactId=helloworld -B -DarchetypeArtifactId=java8-junit4-quickstart -DarchetypeGroupId=org.spilth -Dversion=1.0.0
