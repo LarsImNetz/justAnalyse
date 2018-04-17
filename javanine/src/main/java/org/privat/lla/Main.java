5223efac5462c763dad051f9724618c6af60c1c8
Lars Langhans <lars.langhans@hypoport.de>
2018-04-17 17:02:23 +0200

    Make work again, now we need at least java 9

--- javanine/src/main/java/org/privat/lla/Main.java
+++ javanine/src/main/java/org/privat/lla/Main.java
@@ -1,8 +1,9 @@
+package org.privat.lla;
+
 import java.lang.Process;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Your pid is " + Process.getCurrentPid());
-    }
-    
+    }    
 }
