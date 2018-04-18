e10eeb036d60119d1c39e7b59eb247fb7ef0171d
Lars Langhans <lars.langhans@hypoport.de>
2018-04-18 10:44:13 +0200

    JAva 10 vorbereitet

--- java-ten/src/main/java/org/privat/lla/Main.java
+++ java-ten/src/main/java/org/privat/lla/Main.java
@@ -4,7 +4,7 @@ import java.lang.Process;
 
 public class Main {
     public static void main(String[] args) {
-    	// TODO: Herausfinden, warum getCurrentPid nicht funktioniert.
+        // TODO: Herausfinden, warum getCurrentPid nicht funktioniert
     	// System.out.println("Your pid is " + Process.getCurrentPid());
     }    
 }
---
+++ java-ten/src/test/java/org/privat/lla/atest.java
@@ -0,0 +1,12 @@
+package org.privat.lla;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class atest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
