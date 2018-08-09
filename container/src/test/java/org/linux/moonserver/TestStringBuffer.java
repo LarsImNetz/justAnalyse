39889a50f699622653348edadef7753cbb84b9e5
Lars Langhans <lars.langhans@hypoport.de>
2018-08-09 15:46:25 +0200

    add some tests

--- container/src/test/java/org/linux/moonserver/TestStringBuffer.java
+++ container/src/test/java/org/linux/moonserver/TestStringBuffer.java
@@ -1,5 +1,7 @@
 package org.linux.moonserver;
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,15 +13,52 @@ public class TestStringBuffer {
 	@Before
 	public void setUp() {
 		buffer = new StringBuffer();
-		buffer.append("a");
-		buffer.append("b");
-		buffer.append("c");
+	}
+
+	@Test
+	public void testName() throws Exception {
+		buffer.append("string");
+		buffer.append('a');
+		buffer.append("charArray".toCharArray());
+		buffer.append(true);
+		buffer.append(false);
+		buffer.append(65536);
+		buffer.append(1_000_000_000_000_000_001L);
+		buffer.append(2.7f);
+		buffer.append(3.1415d);
+	
+		Assert.assertEquals("stringacharArraytruefalse6553610000000000000000012.73.1415", buffer.toString());
+	}
+
+	@Test
+	public void testCharAt() throws Exception {
+		buffer.append("Super");
+		Assert.assertEquals('u', buffer.charAt(1));
 	}
 
 	@Test
 	public void testToString() {
+		buffer.append("a");
+		buffer.append("b");
+		buffer.append("c");
 		String all = buffer.toString();
+
 		Assert.assertEquals("abc", all);
 	}
 
+	@Test
+	public void testAppend() throws Exception {
+		buffer.append("hallo");
+		Assert.assertTrue(buffer.toString().contains("hallo"));
+	}
+
+	@Test
+	public void testReverse() throws Exception {
+		buffer.append("a");
+		buffer.append("b");
+		buffer.append("c");
+		buffer.reverse();
+
+		Assert.assertEquals("cba", buffer.toString());
+	}
 }
